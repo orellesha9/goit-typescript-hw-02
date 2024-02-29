ee17a01413ceb32e2588db31847c9a9b7dc4bf8c
orellesha9 <63074265+orellesha9@users.noreply.github.com>
2024-03-01 00:17:57 +0200

    start

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,18 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
-function compare (top, bottom): AllType {
+type CompareType = Pick<AllType, "name" | "position" | "color" | "weight">;
+
+function compare(top: CompareType, bottom: CompareType): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 export {};
